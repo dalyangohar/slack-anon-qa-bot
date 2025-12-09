c38dc5c537ec2075f993a81d577f32b5e4078695
Gohar Dalyan <gohar.dalyan@lightspeedhq.com>
2025-12-09 23:35:08 +0400

    Update bot configuration and dependencies

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(express.json());
